f48e02e42d7127dc4cc49a391e43a52919b5fd51
6VV <6vwei@163.com>
2016-08-17 18:34:05 +0800

    update GeoQuiz

--- app/src/main/java/com/lyyjy/yfyb/androidprogramming/GeoQuiz/QuizActivity.java
+++ app/src/main/java/com/lyyjy/yfyb/androidprogramming/GeoQuiz/QuizActivity.java
@@ -24,19 +24,26 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(this);
         updateQuestionTextView();
 
         findViewById(R.id.next_button).setOnClickListener(this);
         findViewById(R.id.true_button).setOnClickListener(this);
         findViewById(R.id.false_button).setOnClickListener(this);
+        findViewById(R.id.pre_button).setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.next_button:{
+            case R.id.next_button:
+            case R.id.question_text_view:{
                 mCurrentIndex=(mCurrentIndex+1)%mQuestions.length;
+                updateQuestionTextView();
+            }break;
+            case R.id.pre_button:{
+                mCurrentIndex=mCurrentIndex==0?mQuestions.length-1:mCurrentIndex-1;
                 updateQuestionTextView();
             }break;
             case R.id.true_button:{
